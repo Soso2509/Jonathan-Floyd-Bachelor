8eb5b768768ccd9d74c1a14038a17f9aa83f476c
SofieHK <sofiehk@gmail.com>
2024-04-13 11:08:07 +0200

    Basics av å hente fra API

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
-export default function Recipes() {
-  return (
-    <div className="recipe-list">
-      Recipe List
-    </div>
-  )
+import { createClient } from "contentful";
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+
+  const res = await client.getEntries({ content_type: "concert" });
+
+  return {
+    props:{
+      conserts:res.items
+    }
+  }
+}
+
+export default function Index({conserts}) {
+  console.log(conserts)
+  return <div className="recipe-list">
+    {conserts.map(consert =>(
+      <div key ={consert.sys.id}>
+        <h1>{consert.fields.eventName}</h1>
+       {consert.fields.eventDate}
+        </div>
+    ))}
+  </div>;
 }

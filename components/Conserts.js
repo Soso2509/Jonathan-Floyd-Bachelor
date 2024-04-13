98048bb96a248140e76256635c5f5523f7bcaf95
SofieHK <sofiehk@gmail.com>
2024-04-13 18:17:52 +0200

    Fikset i formateringen av dato slik at det gåt DD-MM-YYYY og klokka er 24t

--- components/Conserts.js
+++ components/Conserts.js
@@ -1,14 +1,24 @@
 import Image from "next/image";
+import moment from "moment";
+import 'moment/locale/en-gb';
 
 export default function Conserts({ consert }) {
-  const { eventName, eventDate, slug, eventHeader, eventLocation, ticketsLink} =
-    consert.fields;
+  const {
+    eventName,
+    eventDate,
+    eventTime,
+    slug,
+    eventHeader,
+    eventLocation,
+    ticketsLink,
+  } = consert.fields;
 
+  moment.locale('en-gb')
   return (
     <div>
       <div className="featured">
         <Image
-        className = 'consertImage'
+          className="consertImage"
           src={"https:" + eventHeader.fields.file.url}
           width={eventHeader.fields.file.details.image.width}
           height={eventHeader.fields.file.details.image.height}
@@ -16,7 +26,7 @@ export default function Conserts({ consert }) {
       </div>
       <div className="content">
         <h4>{eventName}</h4>
-        <p>{eventDate}</p>
+        <p>{moment(eventDate).format("LLL")}</p>
         <a href={ticketsLink}>Tickets</a>
       </div>
     </div>

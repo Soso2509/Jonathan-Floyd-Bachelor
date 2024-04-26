a984dc7912341d4dda2fec2e314cc5b87eb4bf0b
SofieHK <sofiehk@gmail.com>
2024-04-26 23:32:11 +0200

    Props for merch vises

--- components/Merch.js
+++ components/Merch.js
@@ -1,34 +1,33 @@
 import Image from "next/image";
 
+export default function Merch({ item }) {
+  const { itemName, itemLink, merchPicture } = item.fields;
 
-export default function Conserts({ consert }) {
-  const {
-    eventName,
-    eventDate,
-    eventLocationLink,
-    eventPlaceName,
-    eventHeader,
-    ticketsLink,
-  } = consert.fields;
-
- 
   return (
     <div>
       <div className="featured">
-        <Image
-          className="consertImage"
-          src={"https:" + eventHeader.fields.file.url}
-          width={eventHeader.fields.file.details.image.width}
-          height={eventHeader.fields.file.details.image.height}
-        />
+        {merchPicture.fields.file.contentType == "video/mp4" ? (
+          <iframe
+            src={"https:" + merchPicture.fields.file.url+"?autostart=false"}
+            frameborder="0"
+            width="50%"
+            height="500"
+            title={merchPicture.fields.description}
+            autoplay="off"
+          />
+        ) : (
+          <Image
+            className="merchImage"
+            src={"https:" + merchPicture.fields.file.url}
+            alt={merchPicture.fields.description}
+            width={merchPicture.fields.file.details.image.width}
+            height={merchPicture.fields.file.details.image.height}
+          />
+        )}
       </div>
       <div className="content">
-        <h4>{eventName}</h4>
-        <p>
-          At <a href={eventLocationLink}>{eventPlaceName}</a>
-        </p>
-        <p>{moment(eventDate).format("LLL")}</p>
-        <a href={ticketsLink}>Tickets</a>
+        <h4>{itemName}</h4>
+        <a href={itemLink}>Buy Here</a>
       </div>
     </div>
   );

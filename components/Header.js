6acd31e9efe4a1edb8d01340c925bdc4ecc8af87
MyconidM <111729616+MyconidM@users.noreply.github.com>
2024-05-07 19:52:44 +0200

    Header finished

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,29 @@
 import Image from "next/image";
 import SoMeLinker from "./SoMeLinker";
 import HeaderImg from "./images/Space-heads.jpeg"
+import HeaderLogo from "./images/Logo-stor.png"
 
 export default function Header() {
-  return <header>
-  <h1>Jonathan Floyd</h1>
-  <SoMeLinker />
+  return <>
+  <div className="headerConteiner">
+    <div className="headerElementer">
+    <Image
+      src={HeaderLogo}
+      className="HeaderLogo"
+      alt="Header Logo som sier Jonatan Floyd"
+    />
+      <SoMeLinker />
+    </div>
+  </div>
+
+  
   <Image
     src={HeaderImg}
     className="HeaderImg"
     alt="Header Img"
+    layout="fill"
+    objectFit="cover"
+    objectPosition="center"
   />
- </header>;
+ </>;
 }

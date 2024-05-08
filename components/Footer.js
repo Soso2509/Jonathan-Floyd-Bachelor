26fbed9dd5644470a37ac4377c27cabe6615be77
MyconidM <111729616+MyconidM@users.noreply.github.com>
2024-05-08 18:36:46 +0200

    Privacu and accsessability pages

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,11 @@
+import Link from "next/link";
 import SoMeLinker from "./SoMeLinker";
 
 export default function Footer() {
   return <>
     <SoMeLinker />
-    <p>Icons by <a target="_blank" href="https://icons8.com">Icons8</a></p>
+    <p className="icon8">Icons by <a target="_blank" href="https://icons8.com">Icons8</a></p>
+    <p>©JonathanFloyd</p>
+    <p><Link href="/privacy">Privacy policy</Link> & <Link href="/accessibility">Accessibility statement</Link></p>
   </>;
 }

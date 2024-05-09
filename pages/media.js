dbaac0ecff2c5e484648c003d816ab5de8b92837
MyconidM <111729616+MyconidM@users.noreply.github.com>
2024-05-09 10:47:43 +0200

    media

--- pages/media.js
+++ pages/media.js
@@ -1,22 +1,4 @@
-import { createClient } from "contentful";
 
-export async function getStaticProps() {
-  const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
-  });
-
-  const bilder = await client.getEntries({ content_type: "gallery" });
-  const video = await client.getEntries({ content_type: "videos" });
-
-  return {
-    props: {
-      gallery: bilder.items,
-      videos: video.items,
-    },
-    revalidate: 1
-  };
-}
 
 export default function Media() {
 

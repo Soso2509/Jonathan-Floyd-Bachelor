32e4303537cfd5db7b25b93bf36035ad311be75d
SofieHK <sofiehk@gmail.com>
2024-05-10 12:09:51 +0200

    Fjernet unødvendlig Iframe kode

--- components/Merch.js
+++ components/Merch.js
@@ -5,31 +5,20 @@ export default function Merch({ item }) {
 
   return (
     <div className="boxContainer">
-      
       <div className="featured">
-        {merchPicture.fields.file.contentType == "video/mp4" ? (
-          <iframe
-            className="boxMedia"
-            src={"https:" + merchPicture.fields.file.url+"?autostart=false"}
-            frameborder="0"
-            width="50%"
-            height="500"
-            title={merchPicture.fields.description}
-            autoplay="off"
-          />
-        ) : (
-          <Image
-            className="boxMedia"
-            src={"https:" + merchPicture.fields.file.url}
-            alt={merchPicture.fields.description}
-            width={merchPicture.fields.file.details.image.width}
-            height={merchPicture.fields.file.details.image.height}
-          />
-        )}
+        <Image
+          className="boxMedia"
+          src={"https:" + merchPicture.fields.file.url}
+          alt={merchPicture.fields.description}
+          width={merchPicture.fields.file.details.image.width}
+          height={merchPicture.fields.file.details.image.height}
+        />
       </div>
       <div className="boxContent">
         <h4>{itemName}</h4>
-        <a className="button" href={itemLink}>Buy Here</a>
+        <a className="button" href={itemLink}>
+          Buy Here
+        </a>
       </div>
     </div>
   );

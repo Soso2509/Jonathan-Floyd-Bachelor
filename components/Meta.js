e29e1267d2b8df8024946b24b8af50d2dd4223c3
MyconidM <111729616+MyconidM@users.noreply.github.com>
2024-05-12 23:11:43 +0200

    Meta tags

--- components/Meta.js
+++ components/Meta.js
@@ -12,12 +12,12 @@ const Meta = () => {
             <meta property="og:locale" content="en_US"/>
             <meta property="og:type" content="website"/>
 
-            <meta name="twitter:card" content="Jonathan_Floyd_Website"/>
+            <meta name="twitter:card" content="summary_large_image"/>
             <meta name="twitter:title" content="Jonathan Floyd"/>
-            <meta name="twitter:description" content="I denne artikkelen får du vite hva Open Graph og Twitter Cards er, og hvordan du kan utnytte disse teknologiene på din nettside."/>
+            <meta name="twitter:description" content="Jonathan Floyd is a multitalented artist and producer from Oslo, Norway."/>
             <meta name="twitter:site" content="@JonathanFloyd"/>
             <meta name="twitter:creator" content="@jonathnFloydOfficial"/>
-            <meta name="twitter:image" content="https://jonathan-floyd-bachelor.vercel.app/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2F27mfh2lrnlh7%2FvR5UNiYxCd3At8OZEXaJr%2F894336cda494d8b8f47beeabf561063d%2F399051107_811398990993307_6476920289215292411_n.jpg&w=1920&q=75"/>
+            <meta name="twitter:image" content="https://jonathan-floyd-bachelor.vercel.app/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2F27mfh2lrnlh7%2FIxd0t2qIjApYoOIJ0ZtcP%2F3c1af19df6cfe90dd86aad9e95eb7d4d%2FKopi_av_Jonathan_Ivar-Tandberg_21.06.2023-3.jpg&w=3840&q=75"/>
             <meta name="twitter:image:alt" content="Image of Jonathan floyd"/>
 
             <meta name="keywords" content="Jonathan Floyd, musician, Når Jeg Faller Ned, Demon Time, Danser på Kanten, Intensjoner, Toothfairy"></meta>

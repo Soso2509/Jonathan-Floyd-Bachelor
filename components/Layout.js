e348976744af7714461abbe98229b60519095a20
MyconidM <111729616+MyconidM@users.noreply.github.com>
2024-05-12 11:54:55 +0200

    div i layout fix

--- components/Layout.js
+++ components/Layout.js
@@ -19,7 +19,7 @@ export default function Layout({ children }) {
           <HamburgerMenu />
         </>
       )}
-      <div>{children}</div>
+      <div className="contentDiv">{children}</div>
 
       <footer>
         <Footer />

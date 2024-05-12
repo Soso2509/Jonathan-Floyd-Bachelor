f20cab93e3aec80c12a97d4050063094aa32c3a9
MyconidM <111729616+MyconidM@users.noreply.github.com>
2024-05-12 22:25:32 +0200

    Meta tags

---
+++ components/Meta.js
@@ -0,0 +1,29 @@
+import Head from "next/head"
+
+const Meta = () => {
+    return (
+        <Head>
+            <meta property="og:title" content="Jonathan Floyd"/>
+            <meta property="og:description" content="Jonathan Floyd is a multitalented artist and producer from Oslo, Norway. With every single release in heavy rotation on NRK P3, Jonathan Floyd has entered the Norwegian music scene with a boom."/>
+            <meta property="og:url" content="https://jonathan-floyd-bachelor.vercel.app/"/>
+            <meta property="og:image" content="https://jonathan-floyd-bachelor.vercel.app/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2F27mfh2lrnlh7%2FvR5UNiYxCd3At8OZEXaJr%2F894336cda494d8b8f47beeabf561063d%2F399051107_811398990993307_6476920289215292411_n.jpg&w=1920&q=75"/>
+            <meta property="og:image:alt" content="Image of Jonathan floyd"/>
+            <meta property="og:site_name" content="Jonathan Floyd"/>
+            <meta property="og:locale" content="en_US"/>
+            <meta property="og:type" content="website"/>
+
+            <meta name="twitter:card" content="Jonathan_Floyd_Website"/>
+            <meta name="twitter:title" content="Jonathan Floyd"/>
+            <meta name="twitter:description" content="I denne artikkelen får du vite hva Open Graph og Twitter Cards er, og hvordan du kan utnytte disse teknologiene på din nettside."/>
+            <meta name="twitter:site" content="@JonathanFloyd"/>
+            <meta name="twitter:creator" content="@jonathnFloydOfficial"/>
+            <meta name="twitter:image" content="https://jonathan-floyd-bachelor.vercel.app/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2F27mfh2lrnlh7%2FvR5UNiYxCd3At8OZEXaJr%2F894336cda494d8b8f47beeabf561063d%2F399051107_811398990993307_6476920289215292411_n.jpg&w=1920&q=75"/>
+            <meta name="twitter:image:alt" content="Image of Jonathan floyd"/>
+
+            <meta name="keywords" content="artist, Jonathan Foyd"></meta>
+            <title>Jonathan Floyd</title>
+        </Head>
+    )
+}
+
+export default Meta
